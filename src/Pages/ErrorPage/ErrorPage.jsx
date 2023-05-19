695afd75efc1bd3f7ee173a03540ab67b109ef47
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-05-19 15:11:31 +0600

    Error page complete

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,35 @@ import ErrPic from "../../../public/imgaes/ErrPage/404.png";
 
 const ErrorPage = () => {
   const { error, status } = useRouteError();
-  return <div className=" rounded-lg border-red-100"></div>;
+  return (
+    <div className=" rounded-lg border-red-100">
+      <section className="flex items-center h-screen p-16 bg-blue-400  text-gray-900">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          {/* <FaceFrownIcon className="w-40 h-40 text-yellow-500" /> */}
+          <div className="max-w-md text-center .flex">
+            <div className="mb-8 .font-extrabold .text-9xl .text-yellow-500">
+              <span className="sr-only w-2/4">Error</span>
+
+              <img src={ErrPic} alt="" />
+              {/* {status || 404} */}
+            </div>
+            <div>
+              <p className="text-2xl  font-semibold md:text-3xl text-red-800 mb-8">
+                Sorry ... <br />
+                {error?.message}
+              </p>
+              <Link
+                to="/"
+                className="btn .btn-outline hover:btn-wide hover:text-white .text-yellow-300 rounded-full"
+              >
+                Back to homepage
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ErrorPage;
